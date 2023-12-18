67855d2308cfeb0a5ecd0373125352506856d323
Tito Yudha Bimashika <tito.yudha.bimashika@gmail.com>
2023-12-19 05:12:55 +0700

    Modified Dashboard Layout

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,8 @@
-<h1>Admin Dashboard</h1>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Admin Dashboard</h1>
+        <!-- Your admin dashboard content here -->
+    </div>
+@endsection
