9caed48b87a9fd6511d6b1ac5b82f35b10aaefe7
Tito Yudha Bimashika <tito.yudha.bimashika@gmail.com>
2023-12-27 23:58:39 +0700

    setup admin routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,36 +19,34 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::middleware(['auth', 'role:user'])->group(function () {
-//     Route::get('/dashboard', function () {
-//         return view('dashboard');
-//     })->name('dashboard');
-// });
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
-
-// Route::middleware(['auth', 'role:admin'])->group(function () {
-//     Route::prefix('admin')->group(function () {
-//         Route::get('/admin/dashboard', 'DashboardController@index')->name('admin.dashboard');
-//     });
-// });
-
-Route::middleware(['auth', 'role:user'])->group(function () {
-    Route::get('/dashboard', [UserController::class, 'checkUserRole'])->name('dashboard');
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'role:user'])->name('dashboard');
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
-    Route::prefix('admin')->group(function () {
-        Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
-    });
+   Route::controller(DashboardController::class)->group(function(){
+        Route::get('/admin/dashboard', 'index');
+   });
+
+   Route::controller(CategoryController::class)->group(function(){
+        Route::get('/admin/all-category', 'index')->name('allcategory');
+        Route::get('/admin/add-category', 'AddCategory')->name('addcategory');
+   });
+
+   Route::controller(SubCategoryController::class)->group(function(){
+        Route::get('/admin/all-subcategory', 'index')->name('allsubcategory');
+        Route::get('/admin/add-subcategory', 'AddSubCategory')->name('addsubcategory');
+   });
+
+   Route::controller(ProductController::class)->group(function(){
+        Route::get('/admin/all-products', 'index')->name('allproducts');
+        Route::get('/admin/add-products', 'AddSubCategory')->name('addproducts');
+   });
+
+   Route::controller(CategoryController::class)->group(function(){
+        Route::get('admin/pending-order', 'index')->name('pendingorder');
+   });
+
 });
-
-
-
-
 
 require __DIR__.'/auth.php';
